3739b406b6c2ffcd9aab1abddf07115bfb89fc4b
Sergey Radzishevskyi <radzserg@gmail.com>
2021-06-09 14:43:59 +0300

    Adds  test

--- tests/__tests__/DbSeeder.test.ts
+++ tests/__tests__/DbSeeder.test.ts
@@ -120,7 +120,7 @@ describe("DbSeeder", () => {
             return { name: "channel_1" };
         });
 
-        it("insert data when referenced field is provided", async () => {
+        it("insert data when referenced field is not provided", async () => {
             const channelsCount: number = await channelsCountInDb();
             const data = await seeder.insert<UserData>("channelAuthors");
             expect(data.name).toEqual("John");
@@ -131,6 +131,22 @@ describe("DbSeeder", () => {
 
             const newChannelsCount: number = await channelsCountInDb();
             expect(newChannelsCount).toBeGreaterThan(channelsCount);
+        });
+
+        it("insert data when referenced field is provided", async () => {
+            await seeder.insert("channel", { id: 2 });
+            const channelsCount: number = await channelsCountInDb();
+            const data = await seeder.insert<UserData>("channelAuthors", {
+                channel_id: 2,
+            });
+            expect(data.name).toEqual("John");
+            expect(data.id).not.toBeNull();
+            expect(data.phone).toEqual("55555555");
+            expect(data.channel_id).toEqual(2);
+            expect(data.foreign_id).toEqual(2132323);
+
+            const newChannelsCount: number = await channelsCountInDb();
+            expect(channelsCount).toEqual(newChannelsCount);
         });
     });
 
